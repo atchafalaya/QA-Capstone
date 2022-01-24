40422f05e94bc47b7d396cce87f069dade65ebb5
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2022-01-24 13:02:03 -0600

    Initial commit

---
+++ selectorsMethods.js
@@ -0,0 +1,19 @@
+const {By, until} = require("selenium-webdriver");
+
+module.exports = class testPage {
+    driver;
+    url;
+    constructor(myDriver, myUrl){
+        this.driver = myDriver
+        this.url = myUrl
+    }
+
+/* SELECTORS */
+
+
+
+
+
+
+/* METHODS */
+}
